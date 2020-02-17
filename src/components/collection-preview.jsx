fc8393c0b3f1815a631fb2a0a72fccc081717e10
Alex J <abel.oalex@gmail.com>
2020-02-17 18:23:37 -0300

    added collection preview and item, and style

--- src/components/collection-preview.jsx
+++ src/components/collection-preview.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import './collection-preview.sass'
 
+import CollectionItem from "./collection-item"
+
 const CollectionPreview =({ title, items }) => (
     <div className='collection-preview'>
         <h1>{title.toUpperCase()}</h1>
-        <div className='preview'>
+        <div key className='preview'>
             {
-                items.filter((item, i) => i<4).map(item => (<div key={item.id}>{item.name}</div>))
+                items.filter((item, i) => i<4).map(({id, ...otherProps}) => (<CollectionItem key={id} {...otherProps} />))
             }
         </div>
     </div>
