5f262ef31c1bf9d1ca67507e1562219612d3e525
Alex J <abel.oalex@gmail.com>
2020-02-17 22:24:31 -0300

    added header component and logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import ShopPage from "./pages/shop";
 
 import HomePage from "./pages/homepage";
+import Header from "./components/header";
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
