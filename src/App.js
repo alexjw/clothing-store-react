47f847e3098b6f0df6aeec7d5d90e4ca9dcb3297
Alex J <abel.oalex@gmail.com>
2020-02-17 23:14:14 -0300

    added sign-in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
-import ShopPage from "./pages/shop";
 
+import ShopPage from "./pages/shop";
 import HomePage from "./pages/homepage";
 import Header from "./components/header";
+import Sign from "./components/sign";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
+            <Route path='/signin' component={Sign}/>
         </Switch>
     </div>
   );
---
+++ src/components/sign.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './sign.sass'
+import SignIn from "./sign-in";
+
+const Sign = () => (
+    <div className='sign'>
+        <SignIn />
+    </div>
+)
+
+export default Sign
