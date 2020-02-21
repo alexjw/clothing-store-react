c2cf5157c8e037961e7c73f06e6dccdd5ad78d5c
Alex J <abel.oalex@gmail.com>
2020-02-21 16:15:04 -0300

    added functionality to checkout

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 import './checkout.sass'
+import {createStructuredSelector} from "reselect";
+import {selectCartItems, selectCartTotal} from "../redux/selectors";
+import { connect } from "react-redux";
 
-const checkoutPage = () => (
-    <div>Checkout Page</div>
+const checkoutPage = ({ cartItems, total }) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'><span>Product</span></div>
+            <div className='header-block'><span>Description</span></div>
+            <div className='header-block'><span>Quantity</span></div>
+            <div className='header-block'><span>Price</span></div>
+            <div className='header-block'><span>Remove</span></div>
+        </div>
+        {
+            cartItems.map(cartItem => cartItem.name)
+        }
+        <div className='total'><span>Total: ${total}</span></div>
+    </div>
 );
 
-export default checkoutPage
+const mapStateToProps = createStructuredSelector(
+    { cartItems: selectCartItems, total: selectCartTotal }
+)
+
+export default connect(mapStateToProps)(checkoutPage)
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,6 +6,8 @@ export const selectCartItems = createSelector([selectCart], (cart) => cart.cartI
 
 export const selectCartItemsCount = createSelector([selectCartItems], cartItems => cartItems.reduce((sum, item) => sum + item.quantity, 0));
 
+export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce((sum, item) => sum + (item.quantity * item.price), 0));
+
 export const selectUser = state => state.user;
 
 export const selectCurrentUser = createSelector([selectUser], (user) => user.currentUser);
