72df09460cb63cc7b244b7941eea3e5ebf0eed38
Alex J <abel.oalex@gmail.com>
2020-02-21 23:31:01 -0300

    toggled cart dropdown when going to checkout

--- src/components/cart-dropdown.jsx
+++ src/components/cart-dropdown.jsx
@@ -7,8 +7,9 @@ import './cart-dropdown.sass'
 import CartItem from "./cart-item";
 import { selectCartItems } from "../redux/selectors";
 import {withRouter} from "react-router-dom";
+import {toggleCartHidden} from "../redux/cart-actions";
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.length  ?
@@ -16,12 +17,17 @@ const CartDropdown = ({ cartItems, history }) => (
                 <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={ () => history.push('/checkout') }>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={ () => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+        } }>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
 const mapStateToProps = state => {
     return {cartItems: selectCartItems(state)}
 };
+
+// If you don't declare mapDispatchToProps on connect, connect passes mapDispatchToProps as prop
 
 export default withRouter(connect(mapStateToProps)(CartDropdown))
