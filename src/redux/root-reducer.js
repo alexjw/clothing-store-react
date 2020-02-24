2970cdf2e4e9ed0809c6bcec59994114a619a671
Alex J <abel.oalex@gmail.com>
2020-02-24 10:59:30 -0300

    added directory reducer to externalize section data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import userReducer from './user-reducer'
 import cartReducer from './cart-reducer'
 import { persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
+import directoryReducer from "./directory-reducer";
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +14,8 @@ const persistConfig = {
 
 const rootReducer =  combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,14 +2,18 @@ import { createSelector } from 'reselect'
 
 const selectCart = state => state.cart;
 
+export const selectUser = state => state.user;
+
+const selectDirectory = state => state.directory;
+
 export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
 
 export const selectCartItemsCount = createSelector([selectCartItems], cartItems => cartItems.reduce((sum, item) => sum + item.quantity, 0));
 
-export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce((sum, item) => sum + (item.quantity * item.price), 0));
+export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden);
 
-export const selectUser = state => state.user;
+export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce((sum, item) => sum + (item.quantity * item.price), 0));
 
 export const selectCurrentUser = createSelector([selectUser], (user) => user.currentUser);
 
-export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden);
+export const selectDirectorySections = createSelector([selectDirectory], directory => directory.sections);
